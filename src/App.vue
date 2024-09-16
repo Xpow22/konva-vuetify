92e2c23f102ed5feaae7f1d78bf6ca8db19e1cd6
Diska Esfandiary Sabilillah <diskaesfandiary22@gmail.com>
2024-09-16 15:55:44 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <link href="https://unpkg.com/@blueprintjs/core@5/lib/css/blueprint.css" rel="stylesheet" />
+    <div id="app"></div>
+  </div>
+</template>
+
+<script>
+import { createDemoApp } from "polotno/polotno-app";
+
+export default {
+  name: "App",
+  mounted() {
+    const { store } = createDemoApp({
+      container: document.getElementById("app"),
+      key: "6633iO-JQydxYQgUQhYr", // you can create it here: https://polotno.com/cabinet/
+      // you can hide back-link on a paid license
+      // but it will be good if you can keep it for Polotno project support
+      showCredit: true,
+    });
+  },
+};
+</script>
+
+<style global>
+#app {
+  width: 100vw;
+  height: 100vh;
+}
+
+body {
+  padding: 0;
+  margin: 0;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,33 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import { aliases, mdi } from 'vuetify/iconsets/mdi-svg'
+import { Icon } from '@iconify/vue'
+import VueKonva from 'vue-konva'
+
+const app = createApp(App)
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi
+    }
+  }
+})
+
+app.use(router)
+app.use(vuetify)
+app.use(VueKonva)
+app.component(Icon)
+
+app.mount('#app')
